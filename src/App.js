ece0c6a800855c1ab15e2dd16a003525a9f61680
Dalton Okechukwu <chike911@yahoo.com>
2020-01-18 02:26:53 -0700

    setup routing to hats page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import React from "react";
 import HomePage from "./pages/homepage.component";
 
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>This is the Hats Page!</h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/hats" component={HatsPage}></Route>
+        </Switch>
       </div>
     );
   }
