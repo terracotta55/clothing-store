5b891120a57dc021e5ff5b0c2eaac5bf793991e2
Dalton Okechukwu <chike911@yahoo.com>
2020-01-18 22:31:33 -0700

    newly created components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-
+/*
 const HatsPage = () => (
   <div>
     <h1>This is the Hats Page!</h1>
   </div>
 );
-
+*/
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
-          <Route path="/hats" component={HatsPage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
         </Switch>
       </div>
     );
