567b40c21413dcc7f6985fc0355eea7b1d7a18e3
Dalton Okechukwu <chike911@yahoo.com>
2020-01-18 22:52:13 -0700

    adding header to app and styling nav a tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
-
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
+
 /*
 const HatsPage = () => (
   <div>
@@ -16,6 +16,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
