b41dccf4132949236d77500038a11c7c9edb71da
Dalton Okechukwu <chike911@yahoo.com>
2020-01-19 00:35:11 -0700

    signin render with with firebase implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 /*
 const HatsPage = () => (
@@ -20,6 +21,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
