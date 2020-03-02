920ff27c637f0a11aa9162b36f3d315c61ede8c5
Dalton Okechukwu <chike911@yahoo.com>
2020-03-01 19:47:42 -0700

    added getCurrentUser method and googleProvider firebase auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -50,7 +50,6 @@ export const addCollectionAndDocuments = async (
   const batch = firestore.batch();
   objectsToAdd.forEach(obj => {
     const newDocRef = collectionRef.doc();
-    console.log(newDocRef);
     batch.set(newDocRef, obj);
   });
 
@@ -68,18 +67,28 @@ export const convertCollectionsSnapshotToMap = collections => {
       items
     };
   });
-  //   console.log("transformedCollection", transformedCollection);
+
   return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
 };
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
