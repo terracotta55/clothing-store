b9d07cc43ed4a813ba188c6dfa7a9e034c2ac440
Dalton Okechukwu <chike911@yahoo.com>
2020-03-01 20:00:27 -0700

    new generator for persistent fetch_collections action

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 
 import {
   firestore,
@@ -12,8 +12,7 @@ import {
 
 import ShopActionTypes from "./shop.types";
 
-// using generator functions
-export function* fetchCollections() {
+export function* fetchCollectionsAsync() {
   try {
     const collectionRef = firestore.collection("collections");
     const snapshot = yield collectionRef.get();
@@ -21,13 +20,19 @@ export function* fetchCollections() {
       convertCollectionsSnapshotToMap,
       snapshot
     );
-    // put is the saga effect for creating dispatch
     yield put(fetchCollectionsSuccess(collectionsMap));
   } catch (error) {
     yield put(fetchCollectionsFailure(error.message));
   }
 }
 
-export function* onFetchCollectionsStart() {
-  yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+export function* fetchCollectionsStart() {
+  yield takeLatest(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
