0ac5116f427550636d9d0ab148d660d825057cc1
Dalton Okechukwu <chike911@yahoo.com>
2020-03-01 19:50:57 -0700

    minor refactor(syntax)

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-  //   collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
